57c6f79960ed2c594d0bb35eb442ae3bdce8b352
Gisele Keimel <gisele.keimel@correlation-one.com>
2024-10-07 17:10:29 -0300

    Added products list

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProductsService {
-
+    products = [];
+    constructor() {
+        let product = {
+            "id": 1,
+            "nombre": "Jabon",
+            "codigo": 23456,
+            "precio": 550
+        }
+        this.products.push(product);
+        product = {
+            "id": 2,
+            "nombre": "Shampoo",
+            "codigo": 12345,
+            "precio": 1300
+        }
+        this.products.push(product);
+    }
     getProducts() {
-        return  [{
-            id: 1,
-            name: 'jabon'
-        },
-        {
-            id: 2,
-            name: 'Shampoo'
-        }];
+        return  this.products;
     }
 
     postProducts(): string {
